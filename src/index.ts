419aca2f66812d149fe5f30684d2e7679e8138eb
pedro <pedroaluz137@gmail.com>
2024-11-10 13:20:33 -0300

    add transalte to pokemon brasil

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,7 @@
-import { fileURLToPath } from "url";
-import path from "path";
-import chalk from "chalk";
 import fs from "fs";
-import fetch from "node-fetch";
 import { getLlama, LlamaChatSession, resolveModelFile } from "node-llama-cpp";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import chalk from "chalk";
+import path from "path";
 
 // IA CONFIG
 const modelsDirectory = path.join(__dirname, "..", "models");
@@ -26,139 +21,283 @@ const session = new LlamaChatSession({
     contextSequence: context.getSequence(),
 });
 
-// Types for various data structures
-interface PokemonData {
-    id: number;
-    name: string;
-    flavor_text_entries: FlavorTextEntry[];
-}
+const formatData = (data: string[]) => {
+    const pokemonList = data.map((pokemon) => {
+        const pokemonData = pokemon.trim().split("\n");
 
-interface FlavorTextEntry {
-    flavor_text: string;
-    language: { name: string };
-    version: { name: string };
-}
+        const pokemonObject = pokemonData.reduce(
+            (
+                acc: Record<
+                    string,
+                    string | string[] | { level: number; move: string }[]
+                >,
+                line
+            ) => {
+                const [key, value] = line.split(" = ");
 
-interface TranslatedPokemon {
-    pokedexId: number;
-    name: string;
-    description: string;
-    descriptionTranslated: string;
-    translatedTime: number;
-}
+                if (key?.includes("[")) {
+                    const index = key.replace("]", "").replace("[", "").trim();
+                    acc.id = index.toString();
+                }
 
-function getPokemonDescription(pokemonData: PokemonData) {
-    const sunOrMoonEntry = pokemonData.flavor_text_entries.find(
-        (entry) =>
-            entry.language.name === "en" &&
-            (entry.version.name === "sun" || entry.version.name === "moon")
-    );
-    if (sunOrMoonEntry) return sunOrMoonEntry.flavor_text;
+                if (key && value) {
+                    switch (key) {
+                        case "Type1":
+                        case "Type2":
+                            const typeTranslations: Record<string, string> = {
+                                FIRE: "FOGO",
+                                WATER: "AGUA",
+                                GRASS: "GRAMA",
+                                ELECTRIC: "ELÉTRICO",
+                                ICE: "GELO",
+                                FIGHTING: "LUTADOR",
+                                POISON: "VENENOSO",
+                                GROUND: "TERRA",
+                                FLYING: "VOADOR",
+                                PSYCHIC: "PSÍQUICO",
+                                BUG: "INSETO",
+                                ROCK: "PEDRA",
+                                GHOST: "FANTASMA",
+                                DRAGON: "DRAGÃO",
+                                DARK: "NOTURNO",
+                                STEEL: "AÇO",
+                                FAIRY: "FADA",
+                                NORMAL: "NORMAL",
+                            };
 
-    const letsGoEntry = pokemonData.flavor_text_entries.find(
-        (entry) =>
-            entry.language.name === "en" &&
-            (entry.version.name === "lets-go-eevee" ||
-                entry.version.name === "lets-go-pikachu")
-    );
-    if (letsGoEntry) return letsGoEntry.flavor_text;
-
-    const lastEntry = pokemonData.flavor_text_entries.find(
-        (entry) => entry.language.name === "en"
-    );
-    return lastEntry ? lastEntry.flavor_text : "Descrição não encontrada";
-}
-
-async function fetchPokemonData(
-    pokemonName: string | number
-): Promise<TranslatedPokemon> {
-    console.log(chalk.yellow("Creating context..."));
-
-    try {
-        console.log(`Buscando dados para ${pokemonName}...`);
-        const response = await fetch(
-            `https://pokeapi.co/api/v2/pokemon-species/${pokemonName
-                .toString()
-                .toLowerCase()}`
-        );
-        if (!response.ok)
-            throw new Error(`Erro ao obter dados para ${pokemonName}`);
-
-        const pokemonData = (await response.json()) as unknown as PokemonData;
-        const description = getPokemonDescription(pokemonData);
-
-        const q1 = `Traduza o seguinte texto do inglês para o português e forneça apenas a tradução: ${description}`;
-
-        const startTime = Date.now();
-
-        console.log(chalk.yellow("User: ") + q1);
-
-        let translatedDescription = "";
-        await session.prompt(q1, {
-            onTextChunk(chunk) {
-                process.stdout.write(chunk);
-                translatedDescription += chunk;
+                            acc[key.trim()] = typeTranslations[value.trim()]!;
+                            break;
+                        case "Moves":
+                            acc[key.trim()] = value
+                                .trim()
+                                .split(",")
+                                .reduce((moves, item, index, array) => {
+                                    if (index % 2 === 0) {
+                                        moves.push({
+                                            level: parseInt(item, 10),
+                                            move: array[index + 1]!,
+                                        });
+                                    }
+                                    return moves;
+                                }, [] as { level: number; move: string }[]);
+                            break;
+                        case "TutorMoves":
+                        case "EggMoves":
+                            acc[key.trim()] = value.trim().split(",");
+                            break;
+                        default:
+                            acc[key.trim()] = value.trim();
+                            break;
+                    }
+                }
+                return acc;
             },
-        });
-        console.log(
-            chalk.yellow("Descrição traduzida: ") + translatedDescription
+            {}
         );
 
-        const endTime = Date.now();
-        const translatedTime = (endTime - startTime) / 1000;
+        return pokemonObject;
+    });
 
-        return {
-            pokedexId: pokemonData.id,
-            name: pokemonData.name,
-            descriptionTranslated: translatedDescription,
-            description,
-            translatedTime,
-        };
-    } catch (error: any) {
-        throw new Error(error.message);
-    }
-}
+    return pokemonList;
+};
 
-function saveToFile(fileName: string, data: any) {
-    const filePath = path.join(__dirname, fileName);
-    fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
-    console.log(`Arquivo salvo em: ${filePath}`);
-}
+const convertToOriginalFormat = (
+    pokemonList: Record<
+        string,
+        string | string[] | number | { level: number; move: string }[]
+    >[]
+) => {
+    return pokemonList
+        .map((pokemon) => {
+            let originalFormat = `[${pokemon.id}]\n`;
 
-async function compareProcess() {
-    const array = Array.from({ length: 12 }, (_, index) => index + 1);
+            for (const [key, value] of Object.entries(pokemon)) {
+                switch (key) {
+                    case "id":
+                        break;
+                    case "Moves":
+                        originalFormat += `${key} = ${(
+                            value as unknown as {
+                                level: number;
+                                move: string;
+                            }[]
+                        )
+                            .map(({ level, move }) => `${level}, ${move}`)
+                            .join(",")}\n`;
+                        break;
+                    case "TutorMoves":
+                    case "EggMoves":
+                        originalFormat += `${key} = ${(
+                            value as unknown as string[]
+                        ).join(",")}\n`;
+                        break;
+                    default:
+                        originalFormat += `${key} = ${value}\n`;
+                        break;
+                }
+            }
 
-    console.log(array);
+            originalFormat += "#-------------------------------\n";
+            return originalFormat;
+        })
+        .join("");
+};
 
+const translate = async (text: string) => {
     const startTime = Date.now();
 
-    const resultsSequential = [];
+    const q1 = `Traduza o seguinte texto do inglês para o português e forneça apenas a tradução: ${text}`;
 
-    for (const item of array) {
-        const data = await fetchPokemonData(item);
-        console.log(data);
-        resultsSequential.push(data);
-    }
+    let translatedText = "";
+    await session.prompt(q1, {
+        onTextChunk(chunk) {
+            translatedText += chunk;
+        },
+    });
+
+    console.log(chalk.yellow("Descrição traduzida: ") + translatedText);
 
     const endTime = Date.now();
+    const translatedTime = (endTime - startTime) / 1000;
 
-    const sequentialTime = (endTime - startTime) / 1000;
+    return { translatedText, translatedTime };
+};
 
-    saveToFile("pokedex.json", {
-        resultsSequential,
-        sequentialTime,
-        maxTime: Math.max(
-            ...resultsSequential.map((result) => result.translatedTime)
-        ),
-        minTime: Math.min(
-            ...resultsSequential.map((result) => result.translatedTime)
-        ),
-        avgTime:
-            resultsSequential.reduce(
-                (acc, curr) => acc + curr.translatedTime,
-                0
-            ) / resultsSequential.length,
-    });
-}
+const analysisFilePath = "./src/tradução/analise.json";
 
-compareProcess();
+const calculateP90 = (times: number[]) => {
+    times.sort((a, b) => a - b);
+    const index = Math.ceil(0.9 * times.length) - 1;
+    return times[index];
+};
+
+const saveAnalysis = (
+    translatedData: {
+        Pokedex: string;
+        Name: string;
+        PokedexTranslated: string;
+        translatedTime: string;
+    }[] = [],
+    range: {
+        min: number;
+        max: number;
+    }
+) => {
+    const translationTimes = translatedData.map((pokemon) =>
+        parseFloat(pokemon.translatedTime as string)
+    );
+    const totalTranslationTime = translationTimes.reduce(
+        (acc, time) => acc + time,
+        0
+    );
+    const averageTranslationTime =
+        totalTranslationTime / translationTimes.length;
+    const p90TranslationTime = calculateP90(translationTimes);
+
+    const analysisData = {
+        date: new Date().toISOString(),
+        totalTranslationTime,
+        averageTranslationTime,
+        p90TranslationTime,
+        translatedPokemonCount: translatedData.length,
+        translationRange: range,
+        translatedPokemon: translatedData,
+        maxTranslatedTime: Math.max(...translationTimes),
+        minTranslatedTime: Math.min(...translationTimes),
+    };
+
+    let existingData = [];
+    if (fs.existsSync(analysisFilePath)) {
+        const fileContent = fs.readFileSync(analysisFilePath, "utf8");
+        existingData = JSON.parse(fileContent);
+    }
+
+    existingData.push(analysisData);
+
+    fs.writeFileSync(analysisFilePath, JSON.stringify(existingData, null, 2));
+};
+
+const main = async (range: { min: number; max: number }) => {
+    const fileContent = fs.readFileSync("./src/pokedex.txt", "utf8");
+    const pokemonList = fileContent
+        .split("#-------------------------------")
+        .slice(range.min - 1, range.max);
+
+    const formattedData = formatData(pokemonList);
+
+    const translatedData: Record<
+        string,
+        string | string[] | { level: number; move: string }[] | number
+    >[] = [];
+
+    const statictics = [];
+
+    let count = range.max - range.min;
+
+    for (const pokemon of formattedData) {
+        const { Pokedex, Name, id } = pokemon as {
+            Pokedex: string;
+            Name: string;
+            id: string;
+        };
+
+        console.log(`Traduzindo ${Name}`);
+
+        const { translatedText, translatedTime } = await translate(Pokedex);
+
+        console.log(`${Name} traduzido em ${translatedTime} segundos`);
+
+        translatedData.push({
+            ...pokemon,
+            id: Number(id),
+            Pokedex: translatedText,
+            PokedexEn: Pokedex,
+        });
+
+        statictics.push({
+            Pokedex,
+            Name,
+            PokedexTranslated: translatedText,
+            translatedTime: translatedTime.toString(),
+        });
+
+        count--;
+
+        console.log(`${count} pokemons restantes`);
+    }
+    const originalFormat = convertToOriginalFormat(translatedData);
+
+    let existingOriginalFormat = "";
+    if (fs.existsSync("./src/tradução/originFormat.txt")) {
+        existingOriginalFormat = fs.readFileSync(
+            "./src/tradução/originFormat.txt",
+            "utf8"
+        );
+    }
+
+    const combinedOriginalFormat =
+        existingOriginalFormat + "\n" + originalFormat;
+
+    fs.writeFileSync("./src/tradução/originFormat.txt", combinedOriginalFormat);
+
+    let existingTranslatedData = [];
+
+    if (fs.existsSync("./src/tradução/formattedData.json")) {
+        const fileContent = fs.readFileSync(
+            "./src/tradução/formattedData.json",
+            "utf8"
+        );
+        existingTranslatedData = JSON.parse(fileContent);
+    }
+
+    existingTranslatedData.push(...translatedData);
+
+    fs.writeFileSync(
+        "./src/tradução/formattedData.json",
+        JSON.stringify(existingTranslatedData, null, 2)
+    );
+
+    saveAnalysis(statictics, range);
+};
+
+main({ min: 203, max: 253 });
